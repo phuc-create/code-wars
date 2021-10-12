59caa0f17b23b6dd0705b05746417623b6410ade
Nguyễn Hữu Phúc <phuc82908@gmail.com>
2021-10-12 11:35:31 +0700

    add kyu7 12/10/2021

--- main.js
+++ main.js
@@ -227,10 +227,9 @@ function mobileKeyboard(str) {
 // freq_seq("19999999", ":"); // => "1:7:7:7:7:7:7:7"
 // freq_seq("^^^**$", "x"); // => "3x3x3x2x2x1"
 function freqSeq(str, sep) {
-  let arr = str.split("");
-  let temp = Array(arr.length).fill(null);
-  for (let i = 0; i < arr.length; i++) {
-    temp[i] = arr.filter((x) => x === arr[i]).length;
+  let temp = Array(str.length).fill(null);
+  for (let i = 0; i < str.length; i++) {
+    temp[i] = arr.filter((x) => x === str[i]).length;
   }
   return temp.join(sep);
 }
@@ -241,3 +240,27 @@ function freqSeq(str, sep) {
     .map((v, i, arr) => arr.filter((vi) => vi === v).length)
     .join(sep);
 }
+// CODE WAR KYU 7
+// Write a function that checks whether all elements in an array are square numbers. The function should be able to take any number of array elements.
+
+// Your function should return true if all elements in the array are square numbers and false if not.
+
+// An empty array should return undefined / None / nil /false (for C). You can assume that all array elements will be positive integers.
+
+// Examples:
+
+// is_square([1, 4, 9, 16]) --> True
+
+// is_square([3, 4, 7, 9]) --> False
+
+// is_square([]) --> None
+const isSquare = (arr) => {
+  if (arr.length > 0) {
+    return arr.every((el) => arr.includes(Math.sqrt(el) * Math.sqrt(el)));
+  }
+  return undefined;
+};
+//ANOTHER SOLUTIONS
+const isSquare = (arr) => {
+  return arr.length ? arr.every((x) => Math.sqrt(x) % 1 === 0) : undefined;
+};
