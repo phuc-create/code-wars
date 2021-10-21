e0a99285327437e3e5fe392ac7d4eeafce331d5b
Nguyễn Hữu Phúc <phuc82908@gmail.com>
2021-10-21 17:25:57 +0700

    add kyu 5 algorithm

--- main.js
+++ main.js
@@ -1089,3 +1089,82 @@ const getPrimes = (num) =>
     .fill()
     .map((e, i) => 2 + i)
     .filter((e, i, a) => a.slice(0, i).every((x) => e % x !== 0));
+//CODE WAR KYU 5
+// The prime numbers are not regularly spaced. For example from 2 to 3 the gap is 1. From 3 to 5 the gap is 2. From 7 to 11 it is 4. Between 2 and 50 we have the following pairs of 2-gaps primes: 3-5, 5-7, 11-13, 17-19, 29-31, 41-43
+// A prime gap of length n is a run of n-1 consecutive composite numbers between two successive primes (see: http://mathworld.wolfram.com/PrimeGaps.html).
+// We will write a function gap with parameters:
+// g (integer >= 2) which indicates the gap we are looking for
+// m (integer > 2) which gives the start of the search (m inclusive)
+// n (integer >= m) which gives the end of the search (n inclusive)
+// n won't go beyond 1100000.
+
+// In the example above gap(2, 3, 50) will return [3, 5] or (3, 5) or {3, 5} which is the first pair between 3 and 50 with a 2-gap.
+// So this function should return the first pair of two prime numbers spaced with a gap of g between the limits m, n if these numbers exist otherwise `nil or null or None or Nothing (or ... depending on the language).
+// In C++, Lua: return in such a case {0, 0}. In F#: return [||]. In Kotlin, Dart and Prolog: return []. In Pascal: return Type TGap (0, 0).
+// Examples:
+// gap(2, 5, 7) --> [5, 7] or (5, 7) or {5, 7}
+// gap(2, 5, 5) --> nil. In C++ {0, 0}. In F# [||]. In Kotlin, Dart and Prolog return []`
+// gap(4, 130, 200) --> [163, 167] or (163, 167) or {163, 167}
+// ([193, 197] is also such a 4-gap primes between 130 and 200 but it's not the first pair)
+// gap(6,100,110) --> nil or {0, 0} or ... : between 100 and 110 we have 101, 103, 107, 109 but 101-107is not a 6-gap because there is 103in between and 103-109is not a 6-gap because there is 107in between.
+// You can see more examples of return in Sample Tests.
+
+function primeSieve(n, m) {
+  let a = Array((n = Math.floor(n / 2))),
+    t = (Math.sqrt(4 + 8 * n) - 2) / 4,
+    u = 0,
+    r = [];
+  for (let i = 1; i < (n - 1) / 3; i++) a[1 + 3 * i] = true;
+  for (let i = 2; i <= t; i++) {
+    u = (n - i) / (1 + 2 * i);
+    if ((i % 3) - 1) for (let j = i; j < u; j++) a[i + j + 2 * i * j] = true;
+  }
+  for (let i = 0; i < n; i++) !a[i] && r.push(i * 2 + 1);
+  return r.filter((num) => num > m - 1);
+}
+const gap = (g, m, n) => {
+  console.log(g, m, n);
+  const primes = primeSieve(n, m);
+  for (let i = 0; i < primes.length; i++) {
+    if (primes[i] + g === primes[i + 1]) {
+      return [primes[i], primes[i + 1]];
+    }
+  }
+  return null;
+};
+//ANOTHER SOLUTION (YOU NEED TO USE MORE ENERGY OF BRAIN)
+function gap(g, m, n) {
+  var lastPrime = 0;
+  var isPrime = function (x) {
+    for (var i = 2; i * i <= x; i++) {
+      if (x % i == 0) return false;
+    }
+    return true;
+  };
+
+  for (var i = m; i <= n; i++)
+    if (isPrime(i)) {
+      if (i - lastPrime == g) return [lastPrime, i];
+      else lastPrime = i;
+    }
+
+  return null;
+}
+
+function isPrime(n) {
+  if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false;
+  var m = Math.sqrt(n);
+  for (var i = 2; i <= m; i++) if (n % i == 0) return false;
+  return true;
+}
+
+function gap(g, m, n) {
+  let mx = 0;
+  for (m, n; m < n; m++) {
+    if (isPrime(m)) {
+      if (m - mx === g) return [mx, m];
+      mx = m;
+    }
+  }
+  return null;
+}
