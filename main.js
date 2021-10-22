7dc724e0375494a1696ec1396e3839c9ce802104
Nguyễn Hữu Phúc <phuc82908@gmail.com>
2021-10-22 09:08:54 +0700

    optimize

--- main.js
+++ main.js
@@ -722,46 +722,34 @@ const fireAndFury = function (tweet) {
 // Playing With Lists/Arrays Series
 // Task
 // Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
-
 // Notes
 // Array/list size is at least 2 .
-
 // Array/list's numbers Will be only Positives
-
 // Repetition of numbers in the array/list could occur.
-
 // Input >> Output Examples
 // productArray ({12,20}) ==>  return {20,12}
 // Explanation:
 // The first element in prod [] array 12 is the product of all array's elements except the first element
-
 // The second element 20 is the product of all array's elements except the second element .
-const productArray = (arr) => {
+function productArray(arr) {
   return arr.map((v, idx) => arr.reduce((a, b) => a * b) / arr[idx]);
-};
+}
 function productArray(numbers) {
   return numbers.map((x) => numbers.reduce((a, b) => a * b) / x);
 }
 
 // CODE WAR KYU 7
 // Given an array of N integers, you have to find how many times you have to add up the smallest numbers in the array until their Sum becomes greater or equal to K.
-
 // Notes:
 // List size is at least 3.
-
 // All numbers will be positive.
-
 // Numbers could occur more than once , (Duplications may exist).
-
 // Threshold K will always be reachable.
-
 // Input >> Output Examples
 // minimumSteps({1, 10, 12, 9, 2, 3}, 6)  ==>  return (2)
 // Explanation:
 // We add two smallest elements (1 + 2), their sum is 3 .
-
 // Then we add the next smallest number to it (3 + 3) , so the sum becomes 6 .
-
 // Now the result is greater or equal to 6 , Hence the output is (2) i.e (2) operations are required to do this .
 
 const minimumSteps = (numbers, value) => {
