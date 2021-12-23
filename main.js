5202df4eff27c7447f02a7e4ca778bf1ddff2c1c
Nguyễn Hữu Phúc <phuc82908@gmail.com>
2021-12-23 08:14:14 +0700

    add kyu 7

--- main.js
+++ main.js
@@ -2458,3 +2458,32 @@ function printerError(s) {
   let errs = (s.match(/[n-z]/g) || []).length
   return `${errs}/${s.length}`
 }
+
+// CODE WAR KYU 7
+// Given an array of integers (x), and a target (t), you must find out if any two consecutive numbers in the array sum to t. If so, remove the second number.
+
+// Example:
+
+// x = [1, 2, 3, 4, 5]
+// t = 3
+
+// 1+2 = t, so remove 2. No other pairs = t, so rest of array remains:
+
+// [1, 3, 4, 5]
+
+// Work through the array from left to right.
+
+// Return the resulting array.
+
+let trouble = (x, t) => {
+  for (var i = 0; i < x.length; i++) {
+    if (x[i] + x[i + 1] === t) {
+      x.splice(i + 1, 1)
+      i--
+    }
+  }
+
+  return x
+}
+// ANOTHER SOLUTIONS
+ trouble = (x, t) => x.reduce((pre, val) => (val + pre[pre.length - 1] !== t ? [...pre, val] : pre), [])
