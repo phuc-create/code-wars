39adbd155a85fff5ec1a1bffcdf4f8f5f7834d7b
Nguyễn Hữu Phúc <phuc82908@gmail.com>
2021-12-28 22:02:47 +0700

    add kyu 7 28/12/2021

--- main.js
+++ main.js
@@ -2710,3 +2710,67 @@ const gcd = (a, b) => (b ? gcd(b, a % b) : a)
 // Hope that helps.
 
 // P.S. There is a risk of an integer overflow within the reduce statement if we have a large array of terms with big numerators and denominators. This can be avoided by dividing the numerators and denominators by the GCD within the reduce statement itself. Since this was a level 6 problem, I didn't feel that was necessary but it's still worth noting.
+
+// CODE WAR KYU 7 (BUT IT IS FUNNY)
+// Description:
+// John is a worker, his job is to remove screws from a machine. There are 2 types of screws: slotted (-) and cross (+). John has two screwdrivers, one for each type of screw.
+
+// The input will be a (non-empty) string of screws, e.g. : "---+++"
+
+// When John begins to work, he stands at the first screw, with the correct screwdriver in his hand, and another in his tool kit. He works from left to right, removing every screw. When necessary, he switches between the screwdriver in his hand and the one in his tool kit.
+
+// Each action takes a set amount of time:
+
+// remove one screw : 1 second
+// move to the next screw: 1 second
+// switch screwdrivers: 5 seconds
+// Your task is to return the total time taken to remove all the screws, in seconds.
+
+// Examples
+// In order to be more clear, we use ABCDEF to represent the screws. The number in brackets is the time in seconds:
+
+// screws: "---+++"
+//          ABCDEF
+
+// remove A (1) + move to B (1) + remove B (1) +
+// move to C (1) + remove C (1) + move to D (1) +
+// switch screwdriver (5) + remove D (1) +
+// move to E (1) + remove E (1) + move to F (1) + remove F (1)
+
+// total time = 16 seconds
+// Another example:
+
+// screws: "-+-+-+"
+//          ABCDEF
+
+// remove A (1) +
+// move to B (1) + switch screwdriver (5) + remove B (1) +
+// move to C (1) + switch screwdriver (5) + remove C (1) +
+// move to D (1) + switch screwdriver (5) + remove D (1) +
+// move to E (1) + switch screwdriver (5) + remove E (1) +
+// move to F (1) + switch screwdriver (5) + remove F (1)
+
+// total time = 36 seconds
+function sc(screws) {
+  screws = screws.split('')
+  let count = 0
+  for (let i = 0; i < screws.length - 1; i++) {
+    if (screws[i] !== screws[i + 1]) {
+      count += 7
+    } else {
+      count += 2
+    }
+  }
+  return count + 1
+}
+// ANOTHER SOLUTIONS
+
+sc = (screws) => screws.split('').reduce((time, screw, i) => (screw == screws[i + 1] ? time + 2 : time + 7), 1) - 7
+
+sc = (screws) => {
+  let sum = (2 * screws.length) - 1
+   for (let i = 0; i < screws.length - 1; i++) {
+       if (screws[i] != screws[i + 1]) { sum += 5 }
+     }
+    return sum
+  }
