1f6d32098028d2bf27c45b5b538472cd9988cb72
Nguyễn Hữu Phúc <phuc82908@gmail.com>
2022-01-05 14:00:33 +0700

    add another kyu 6 05/01/2022

--- main.js
+++ main.js
@@ -2817,3 +2817,99 @@ multiplicationTable = function (size) {
 multiplicationTable = function (n) {
   return Array.from({ length: n }, (_, i) => Array.from({ length: n }, (_, j) => (i + 1) * (j + 1)))
 }
+
+// CODE WAR KYU 6
+// Description:
+// Let us begin with an example:
+
+// A man has a rather old car being worth $2000. He saw a secondhand car being worth $8000. He wants to keep his old car until he can buy the secondhand one.
+
+// He thinks he can save $1000 each month but the prices of his old car and of the new one decrease of 1.5 percent per month. Furthermore this percent of loss increases of 0.5 percent at the end of every two months. Our man finds it difficult to make all these calculations.
+
+// Can you help him?
+
+// How many months will it take him to save up enough money to buy the car he wants, and how much money will he have left over?
+
+// Parameters and return of function:
+
+// parameter (positive int or float, guaranteed) startPriceOld (Old car price)
+// parameter (positive int or float, guaranteed) startPriceNew (New car price)
+// parameter (positive int or float, guaranteed) savingperMonth
+// parameter (positive float or int, guaranteed) percentLossByMonth
+
+// nbMonths(2000, 8000, 1000, 1.5) should return [6, 766] or (6, 766)
+// Detail of the above example:
+// end month 1: percentLoss 1.5 available -4910.0
+// end month 2: percentLoss 2.0 available -3791.7999...
+// end month 3: percentLoss 2.0 available -2675.964
+// end month 4: percentLoss 2.5 available -1534.06489...
+// end month 5: percentLoss 2.5 available -395.71327...
+// end month 6: percentLoss 3.0 available 766.158120825...
+// return [6, 766] or (6, 766)
+// where 6 is the number of months at the end of which he can buy the new car and 766 is the nearest integer to 766.158... (rounding 766.158 gives 766).
+
+// Note:
+
+// Selling, buying and saving are normally done at end of month. Calculations are processed at the end of each considered month but if, by chance from the start, the value of the old car is bigger than the value of the new one or equal there is no saving to be made, no need to wait so he can at the beginning of the month buy the new car:
+
+// nbMonths(12000, 8000, 1000, 1.5) should return [0, 4000]
+// nbMonths(8000, 8000, 1000, 1.5) should return [0, 0]
+// We don't take care of a deposit of savings in a bank:-)
+function calcPrice(percent, num) {
+  return ((100 - percent) * num / 100)
+}
+
+function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth) {
+  let save = savingperMonth
+  let loss = percentLossByMonth
+  let oldcar = startPriceOld
+  let newcar = startPriceNew
+  let avail = 0
+  let inc = 0.5
+  let countMonth = 1
+
+  if (oldcar >= newcar) return [0, oldcar - newcar]
+
+  while (avail <= 0) {
+    oldcar = calcPrice(loss, oldcar)
+    newcar = calcPrice(loss, newcar)
+
+    avail = (oldcar + save) - newcar
+
+    countMonth++
+    save += savingperMonth
+
+    if (countMonth % 2 === 0) {
+      loss += inc
+    }
+  }
+  return [countMonth - 1, Math.round(avail)]
+}
+// ANOTHER SOLUTIONS
+ nbMonths = (startPriceOld, startPriceNew, savingperMonth, percentLossByMonth) => {
+  var months = 0,
+moneySaved = 0
+  while (startPriceNew > startPriceOld + moneySaved) {
+    moneySaved += savingperMonth
+    startPriceOld -= (startPriceOld * (percentLossByMonth / 100))
+    startPriceNew -= (startPriceNew * (percentLossByMonth / 100))
+    months++
+    if (months % 2 == 1) {
+      percentLossByMonth += 0.5
+    }
+  }
+  return [months, Math.round(startPriceOld + moneySaved - startPriceNew)]
+}
+
+nbMonths = (myCar, newCar, perMonth, loss) => {
+  loss = 1 - (loss / 100)
+  var w = myCar
+  for (var m = 0; w < newCar; m++) {
+    loss -= m % 2 == 0 ? 0 : 0.005
+    myCar *= loss
+    newCar *= loss
+    w = myCar + perMonth * (m + 1)
+  }
+  return [m, Math.round(w - newCar)]
+}
+// hi
